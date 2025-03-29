737ad7a6b305f78a4aebddb9433a39728b17e597
nunomoreira1993 <101346099+nunomoreira1993@users.noreply.github.com>
2025-03-29 14:02:20 +0000

    Update barcode.php

--- rp/barcode.php
+++ rp/barcode.php
@@ -3,7 +3,7 @@ include $_SERVER["DOCUMENT_ROOT"] . "/plugins/phpbarcode/barcode.php";
 
 $generator = new barcode_generator();
 $s = $_GET["s"];
-$d = $_GET["d"];
+$d = (int) $_GET["d"];
 /* Generate SVG markup and write to standard output. */
 header('Content-Type: image/svg+xml');
 $svg = $generator->render_svg($s, $d, $options);
